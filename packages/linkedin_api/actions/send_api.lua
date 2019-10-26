bd8a137a15671c0c9b3aaf8f56acfe4b6f5b0ffa
Luis Reyes <luisrogelio98@gmail.com>
2019-10-25 23:49:43 -0400

    example data request WIP

--- packages/linkedin_api/actions/send_api.lua
+++ packages/linkedin_api/actions/send_api.lua
@@ -2,12 +2,25 @@ event = ["send_linkedin"]
 priority = 3
 input_parameters = ["request"]
 
+--  GET https://api.linkedin.com/v2/people/(id:{person ID})
+
+--  GET https://api.linkedin.com/v2/me
+
+-- local github_response = send_request({
+--     uri='',
+--     method="get",
+--     headers={
+--         ["content-type"]="application/json",
+--     },
+-- })
+
 response = {
     headers = {
         ["content-type"] = "text/html",
     },
     body = render("index.html", {
-        dev = "Under development"
+        dev = "Under development",
+        api_sample = settings.api_sample.body
     })
 }
 
